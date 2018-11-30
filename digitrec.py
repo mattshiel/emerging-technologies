469576ea3cbe7921f77ec3d9ac404d4e3e630afb
Matthew Shiel <mattshiel@gmail.com>
2018-11-30 21:52:43 +0000

    Added basic menu to be expanded on

--- digitrec.py
+++ digitrec.py
@@ -4,6 +4,7 @@
 
 # Imports
 import os.path as path
+import numpy as np
 
 # Library used to prompt user for file
 # dialog that requests selection of an existing file.
@@ -14,53 +15,67 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.python.keras.models import save_model
 
-# Store 28x28 images of handwritten digits 0-9 from MNIST dataset
-mnist = tf.keras.datasets.mnist 
+def loadClassifier():
+    # Store 28x28 images of handwritten digits 0-9 from MNIST dataset
+    mnist = tf.keras.datasets.mnist 
 
-# Load the dataset in to memory
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # Load the dataset in to memory
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Normalize the data
-# In this case rescaling the data
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+    # Normalize the data
+    # In this case rescaling the data
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-#2 Types of models, Sequential is the most common
-model = tf.keras.models.Sequential()
+    #2 Types of models, Sequential is the most common
+    model = tf.keras.models.Sequential()
 
-# Often times at the end of a CNN there will be a densely connected layer which needs to be flattened. It gets the output of the 
-# convolutional layers, flattens all of its structure to create a single long feature vector to be used by the 
-# dense layer for the final classification.
-model.add(tf.keras.layers.Flatten())
+    # Often times at the end of a CNN there will be a densely connected layer which needs to be flattened. It gets the output of the 
+    # convolutional layers, flattens all of its structure to create a single long feature vector to be used by the 
+    # dense layer for the final classification.
+    model.add(tf.keras.layers.Flatten())
 
-# Plot digit to make sure everything is working
-plt.imshow(x_train[0])
-plt.show()
+    # Plot digit to make sure everything is working
+    plt.imshow(x_train[0])
+    plt.show()
 
-# Activation is what makes the neuron fire
-# tf.nn.relu is a very standard default activation function, it can be tweaked to possibly improve performance
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-# This layer is the output layer, the output layer will always have the number of classifications, 10 in this case
-model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+    # Activation is what makes the neuron fire
+    # tf.nn.relu is a very standard default activation function, it can be tweaked to possibly improve performance
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    # This layer is the output layer, the output layer will always have the number of classifications, 10 in this case
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-# Loss is, in simple terms, the degree of error
-# Neural Networks aren't necessarily trying to maximise efficiency but minimize loss
-model.compile(optimizer ='adam',
-             loss='sparse_categorical_crossentropy',
-             metrics=['accuracy'])
+    # Loss is, in simple terms, the degree of error
+    # Neural Networks aren't necessarily trying to maximise efficiency but minimize loss
+    model.compile(optimizer ='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-#Train the model
-#An epoch is a full pass through the entire dataset
-model.fit(x_train, y_train, epochs=3)
+    #Train the model
+    #An epoch is a full pass through the entire dataset
+    model.fit(x_train, y_train, epochs=3)
 
-# File explorer image selection
-# while True:
+def userMenu():
+    ans=True
+    while ans:
+        print ("""
+        ======================
+        MNIST IMAGE CLASSIFIER
+        ======================
+        1. Load Classifier
+        2. Make prediction
+        3.Exit
 
-#     # Not necessary to explicitly have this line but it's good practice
-#     root = tk.Tk()
-#     root.withdraw()
+        """)
+        ans=input("Please make a selection: ") 
+        if ans=="1": 
+            loadClassifier()
+        elif ans=="2":
+            print("\n Exiting...")
+            exit()
+        elif ans !="":
+            print("\n Not Valid Choice Try again") 
 
-#     # Adapted from https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
-#     file_path = filedialog.askopenfilename()
 
+userMenu()
