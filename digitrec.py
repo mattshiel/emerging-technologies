dc4988d16665f38d462a36ff8d10a280a8a80e33
Matthew Shiel <mattshiel@gmail.com>
2018-11-30 20:36:27 +0000

    Load MNIST into memory with keras and tf, then tested plot to ensure everything is working

--- digitrec.py
+++ digitrec.py
@@ -4,26 +4,32 @@
 
 # Imports
 import os.path as path
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.python.keras.models import save_model
 
 # Library used to prompt user for file
 # dialog that requests selection of an existing file.
 import tkinter as tk
 from tkinter import filedialog
 
-# def getImage() {
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.keras.models import save_model
 
-# }
+# Store 28x28 images of handwritten digits 0-9 from MNIST dataset
+mnist = tf.keras.datasets.mnist 
 
+# Load the dataset in to memory
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Plot digit to make sure everything is working
+plt.imshow(x_train[0])
+plt.show()
 # File explorer image selection
-while True:
+# while True:
 
-    # Not necessary to explicitly have this line but it's good practice
-    root = tk.Tk()
-    root.withdraw()
+#     # Not necessary to explicitly have this line but it's good practice
+#     root = tk.Tk()
+#     root.withdraw()
 
-    # Adapted from https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
-    file_path = filedialog.askopenfilename()
+#     # Adapted from https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
+#     file_path = filedialog.askopenfilename()
 
