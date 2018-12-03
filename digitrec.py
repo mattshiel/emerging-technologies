7996aeb2c69f95de15f56d81a60f6f34b1c3a7ab
Matthew Shiel <mattshiel@gmail.com>
2018-12-03 09:17:39 +0000

    Cleaned up code and structured it better

--- digitrec.py
+++ digitrec.py
@@ -4,7 +4,6 @@
 
 # Imports
 import os.path as path
-import numpy as np
 
 # Library used to prompt user for file
 # dialog that requests selection of an existing file.
@@ -12,10 +11,16 @@ import tkinter as tk
 from tkinter import filedialog
 
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras.models import save_model
 
-def loadClassifier():
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+MODEL_NAME = "digitClassifier.h5"
+
+def prepDataset():
     # Store 28x28 images of handwritten digits 0-9 from MNIST dataset
     mnist = tf.keras.datasets.mnist 
 
@@ -27,6 +32,12 @@ def loadClassifier():
     x_train = tf.keras.utils.normalize(x_train, axis=1)
     x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+    return x_train, y_train, x_test, y_test
+
+def loadClassifier(MNIST_data):
+    
+    x_train, y_train, x_test, y_test = MNIST_data
+
     #2 Types of models, Sequential is the most common
     model = tf.keras.models.Sequential()
 
@@ -34,10 +45,6 @@ def loadClassifier():
     # convolutional layers, flattens all of its structure to create a single long feature vector to be used by the 
     # dense layer for the final classification.
     model.add(tf.keras.layers.Flatten())
-
-    # Plot digit to make sure everything is working
-    plt.imshow(x_train[0])
-    plt.show()
 
     # Activation is what makes the neuron fire
     # tf.nn.relu is a very standard default activation function, it can be tweaked to possibly improve performance
@@ -56,6 +63,36 @@ def loadClassifier():
     #An epoch is a full pass through the entire dataset
     model.fit(x_train, y_train, epochs=3)
 
+    # Calculate and display the loss and model accuracy 
+    val_loss, val_acc = model.evaluate(x_test, y_test)
+    print(val_loss, val_acc)
+
+    # Save the model
+    model.save(MODEL_NAME)
+    print('Saved model as ' + MODEL_NAME)
+
+
+def makePrediction(MNIST_data):
+
+    # Get the data necessary to test
+    x_train, y_train, x_test, y_test = MNIST_data
+
+    # Load the model
+    active_model = tf.keras.models.load_model(MODEL_NAME)
+
+    # Generate a random number between 1-10000
+    randTestImg = np.random.randint(1,10000)
+
+    # Make predictions 
+    predictions = active_model.predict(x_test)
+
+    # Print out prediction
+    print(np.argmax(predictions[randTestImg]))
+
+    # Show what the number actually was
+    plt.imshow(x_test[randTestImg])
+    plt.show()
+
 def userMenu():
     ans=True
     while ans:
@@ -63,16 +100,19 @@ def userMenu():
         ======================
         MNIST IMAGE CLASSIFIER
         ======================
-        1. Load Classifier
+        1. Create and Load Basic Classifier
         2. Make prediction
         3.Exit
 
         """)
-        ans=input("Please make a selection: ") 
+        ans=input("Please make a selection: ")
+
+        MNIST_data = prepDataset()
+ 
         if ans=="1": 
-            loadClassifier()
+            loadClassifier(MNIST_data)
         elif ans=="2":
-            print("\n Exiting...")
+            makePrediction(MNIST_data)
             exit()
         elif ans !="":
             print("\n Not Valid Choice Try again") 
